7f8e16048d7e835bddf69d87868d094b9b453802
achuavi <72316433+achuavi@users.noreply.github.com>
2022-04-20 10:24:11 +0530

    Add files via upload

---
+++ d2DISPLAY.java
@@ -0,0 +1,15 @@
+package p3java;
+
+import java.util.Scanner;
+
+public class d2DISPLAY {
+
+	public static void main(String[] args) {
+		String str;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the string");
+		str=sc.next();
+		System.out.println("The String is\t"+str);
+	}
+
+}
